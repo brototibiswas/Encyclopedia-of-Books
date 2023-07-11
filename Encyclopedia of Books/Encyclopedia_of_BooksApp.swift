a55f8b0c66282e5a30a5ffdd0985d855778fb0e5
Brototi Biswas <72146916+brototibiswas@users.noreply.github.com>
2023-07-10 21:49:01 -0400

    Initial Commit

---
+++ Encyclopedia of Books/Encyclopedia_of_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Encyclopedia_of_BooksApp.swift
+//  Encyclopedia of Books
+//
+//  Created by Brototi Biswas on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Encyclopedia_of_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
